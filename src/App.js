68eee8daecd09fb385e56cb724f86128fade2ddd
Nicofisi <m.lomiak@gmail.com>
2021-11-30 16:02:28 +0100

    Move components to their own directories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeRoute from './routes/HomeRoute';
-import TermRoute from './routes/TermRoute';
-import DashboardRoute from './routes/DashboardRoute';
-import AddTermRoute from './routes/dashboard/AddTermRoute';
-import EditTermRoute from './routes/dashboard/EditTermRoute';
-import DashboardHomeRoute from './routes/dashboard/DashboardHomeRoute';
-import NotFoundRoute from './routes/NotFoundRoute';
+import HomeRoute from './routes/HomeRoute/HomeRoute';
+import TermRoute from './routes/TermRoute/TermRoute';
+import DashboardRoute from './routes/DashboardRoute/DashboardRoute';
+import AddTermRoute from './routes/dashboard/AddTermRoute/AddTermRoute';
+import EditTermRoute from './routes/dashboard/EditTermRoute/EditTermRoute';
+import DashboardHomeRoute from './routes/dashboard/DashboardHomeRoute/DashboardHomeRoute';
+import NotFoundRoute from './routes/NotFoundRoute/NotFoundRoute';
 
 function App() {
   return (
