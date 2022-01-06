a0af751740919aaa123cee749d428d2ca996269e
Nicofisi <m.lomiak@gmail.com>
2022-01-06 19:35:12 +0100

    Introduce initial Not Found route design

--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,5 +1,25 @@
+import styles from './NotFoundRoute.module.css';
+import Header from '../../components/Header/Header';
+import {Link, useLocation} from 'react-router-dom';
+
 const NotFoundRoute = () => {
-  return "not found";
+  const location = useLocation();
+
+  return (
+      <div className={styles.notFound}>
+        <Header />
+        <div className={styles.mainContainer}>
+          <main className={styles.main}>
+            <h1>404 Nie ma takiej strony - {location.pathname}</h1>
+            <p>
+              <Link to="/">
+                Przejdź do strony głównej
+              </Link> lub wyszukaj w polu wyżej
+            </p>
+          </main>
+        </div>
+      </div>
+  );
 };
 
 export default NotFoundRoute;
