044ecea66689465b44eaaa52ffad9b6478e9c34a
Nicofisi <m.lomiak@gmail.com>
2022-01-08 00:11:36 +0100

    Add missing title to home route

--- src/routes/HomeRoute/HomeRoute.js
+++ src/routes/HomeRoute/HomeRoute.js
@@ -1,9 +1,13 @@
 import styles from './HomeRoute.module.css'
 import Header from '../../components/Header/Header';
+import {Helmet} from 'react-helmet-async';
 
 const HomeRoute = () => {
   return (
     <div className={styles.home}>
+      <Helmet>
+        <title>Słownik Biologiczny</title>
+      </Helmet>
       <Header />
     </div>
   );
