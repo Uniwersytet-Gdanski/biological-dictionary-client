47031df8adc3a7262d50cb1f9a5715ece5d8200a
Rafał Majewski <goodheropl@gmail.com>
2022-01-22 00:46:47 +0100

    use configure store from redux toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,9 @@
-// import { configureStore } from '@reduxjs/toolkit';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import termsReducer from '../slices/terms.js';
-import thunk from 'redux-thunk';
 
-// export const store = configureStore({
-//   reducer: {
-//     blah: () => null
-//   },
-// });
 
-export const store = createStore(
-	combineReducers({
-		terms: termsReducer,
-	}),
-	applyMiddleware(thunk),
-);
+export const store = configureStore({
+  reducer: {
+    terms: termsReducer,
+  },
+});
