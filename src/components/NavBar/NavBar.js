68263dfb97c884a9f02271d6baaf8a902d84334c
tusiaa <sandra.leman146@gmail.com>
2022-01-22 23:05:17 +0100

    Add icons to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,25 @@
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { IoPersonCircle } from 'react-icons/io5';
+import { IoLogOut } from 'react-icons/io5';
 import { getUser } from '../../redux/slices/user';
 
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
-  const user = useSelector(getUser);  // TODO: logout button and going to dashboard
+  const user = useSelector(getUser);  // TODO: logout button
   return (
     <nav className={styles.bar}>
       {user && (
         <div>
-          📴 {user.username}
+        <NavLink to="/dashboard">
+          < IoPersonCircle className={styles.icon} /> 
+          {user.username}
+        </NavLink>
+        <NavLink to="/dashboard">
+          < IoLogOut className={styles.icon} /> 
+          Wyloguj
+        </NavLink>
         </div>
       )}
       <NavLink to="/about">O słowniku</NavLink>
