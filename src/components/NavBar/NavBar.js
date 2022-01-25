e97c62910a6d9dc1ec05d0b467a1ec9f7ec5b6a0
tusiaa <sandra.leman146@gmail.com>
2022-01-25 03:22:15 +0100

    Update links in NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,39 @@
-import { IoLogOut, IoPersonCircle } from 'react-icons/io5';
+import { IoLogOut, IoPersonCircle, IoAddCircleSharp } from 'react-icons/io5';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { getUser } from '../../redux/slices/user';
+// import axiosClient from '../../axiosClient';
+// import { setUser } from '../../redux/slices/user';
 
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
-  const user = useSelector(getUser);  // TODO: logout button
+  const user = useSelector(getUser);  
+
+  const logOut = () => {
+    // axiosClient.post(`/logout`)
+    // .then(_ => {
+    //   console.log("logout successful");
+    //   dispatch(setUser(null));
+    // }).catch(ex => {
+    //   console.error(ex);
+    //   alert("Logout failed");  // TODO: better error handling
+    // });
+  };
+
   return (
     <nav className={styles.bar}>
       {user && (
         <div>
-          <NavLink to="/dashboard">
-            <IoPersonCircle className={styles.icon} />
-            {user.username}
-          </NavLink>
-          <NavLink to="/dashboard">
+          <IoPersonCircle className={styles.icon} />
+          {user.username}
+          <NavLink onClick={logOut} to="/">
             <IoLogOut className={styles.icon} />
             Wyloguj
+          </NavLink>
+          <NavLink to="/terms/add">
+            < IoAddCircleSharp className={styles.icon} />
+            Dodaj
           </NavLink>
         </div>
       )}
