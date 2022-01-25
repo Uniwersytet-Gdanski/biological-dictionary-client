68e34a8d17f403ff26e2af9e1d35e66dc235837f
tusiaa <sandra.leman146@gmail.com>
2022-01-25 17:37:22 +0100

    Refactor Authors and Contact Routes

--- src/routes/info/AuthorsRoute/AuthorsRoute.js
+++ src/routes/info/AuthorsRoute/AuthorsRoute.js
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import Authors from '../../../components/Authors/Authors';
+import HeaderWithoutSearch from '../../../components/HeaderWithoutSearch/HeaderWithoutSearch';
 import styles from './AuthorsRoute.module.css'
 
 const AuthorsRoute = () => {
@@ -8,7 +8,35 @@ const AuthorsRoute = () => {
       <Helmet>
         <title> O słowniku - Słownik Biologiczny </title>
       </Helmet>
-      <Authors />
+      <HeaderWithoutSearch />
+
+      <div className={styles.main}>
+        <h1 className={styles.title}>Autorzy</h1>
+        <p>
+          Polsko-angielski słownik terminów biologicznych, pierwszy tego typu słownik w Polsce, został przygotowany przez 
+          następujących studentów Genetyki i Biologii Eksperymentalnej z Wydziału Biologii Uniwersytetu Gdańskiego:
+          <ul>
+            <li>Kacpra Boguszewskiego</li>
+            <li>Katarzynę Bryszkowską</li>
+            <li>Aleksandrę Kujałowicz</li>
+            <li>Marcina Banackiego</li>
+            <li>Huberta Czyża</li>
+            <li>Annę Barczak</li>
+          </ul>
+          Pod opieką mgr Barbary Kubicy-Daniel z Centrum Języków Obcych Uniwersytetu Gdańskiego.
+        </p>
+        <p>
+          Za przygotowanie strony internetowej odpowiedzialny jest zespół studentów Wydziału Matematyki, Fizyki i Informatyki 
+          pod opieką dr Hanny Furmańskiej i mgr Wojciecha Łojkowskiego w składzie:
+          <ul>
+            <li>Rafał Majewski</li>
+            <li>Mikołaj Lomiak</li>
+            <li>Sandra Leman</li>
+          </ul>
+          Za opracowanie interfejsu odpowiedzialna jest Joanna Jaworska, studentka studiów magisterskich 
+          z Wydziału Nauk Społecznych Uniwersytetu Gdańskiego.
+        </p>
+      </div>
     </div>
   );
 };
--- src/routes/info/ContactRoute/ContactRoute.js
+++ src/routes/info/ContactRoute/ContactRoute.js
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import Contact from '../../../components/Contact/Contact';
+import HeaderWithoutSearch from '../../../components/HeaderWithoutSearch/HeaderWithoutSearch';
 import styles from './ContactRoute.module.css'
 
 const ContactRoute = () => {
@@ -8,7 +8,18 @@ const ContactRoute = () => {
       <Helmet>
         <title> O słowniku - Słownik Biologiczny </title>
       </Helmet>
-      <Contact />
+      <HeaderWithoutSearch />
+
+      <div className={styles.main}>
+        <h1 className={styles.title}>Kontakt</h1>
+        <p className={styles.contact}>
+          Kontakt z autorami dostępny mailowo: 
+          <a className={styles.email} href='mailto:slownikbiologia@gmail.com'>slownikbiologia@gmail.com</a>
+        </p>
+        <p className={styles.contact}>
+          <a href='https://forms.gle/ZFwLMs7FxL1RBT9A8'>Zgłoś błąd / zaproponuj nowy termin </a>
+        </p>
+      </div>
     </div>
   );
 };
