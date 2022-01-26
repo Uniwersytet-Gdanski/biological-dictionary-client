268f38afc028480c84a785bd99a5b986d797ebc7
tusiaa <sandra.leman146@gmail.com>
2022-01-26 23:12:30 +0100

    Add login verification in AddTermRoute

--- src/routes/AddTermRoute/AddTermRoute.js
+++ src/routes/AddTermRoute/AddTermRoute.js
@@ -2,8 +2,12 @@ import { Helmet } from 'react-helmet-async';
 import HeaderWithSearch from '../../components/HeaderWithSearch/HeaderWithSearch';
 import TermForm from '../../components/term/TermForm/TermForm';
 import styles from './AddTermRoute.module.css';
+import { useSelector } from 'react-redux';
+import { getUser } from '../../redux/slices/user';
 
 const AddTermRoute = () => {
+  const user = useSelector(getUser);
+
   return (
     <div className={styles.route}>
       <Helmet>
@@ -13,7 +17,7 @@ const AddTermRoute = () => {
       </Helmet>
       <HeaderWithSearch />
       <main className={styles.main}>
-        <TermForm />
+        { user ? <TermForm /> : <h2 className={styles.warning}> Musisz być zalogowany. </h2>}
       </main>
     </div>
   );
