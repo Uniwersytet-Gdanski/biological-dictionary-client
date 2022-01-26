9684a45c5f8901a4de2b9fe1b9ef01642c6a8c0e
tusiaa <sandra.leman146@gmail.com>
2022-01-26 22:59:27 +0100

    Add login verification in TermEditRoute

--- src/routes/TermRoute/TermEditRoute/TermEditRoute.js
+++ src/routes/TermRoute/TermEditRoute/TermEditRoute.js
@@ -1,9 +1,13 @@
 import { Helmet } from 'react-helmet-async';
 import { useOutletContext } from 'react-router-dom';
 import TermForm from '../../../components/term/TermForm/TermForm';
+import { useSelector } from 'react-redux';
+import { getUser } from '../../../redux/slices/user';
+import styles from './TermEditRoute.module.css'
 
 const TermEditRoute = () => {
   const [termId, term, savedQuery] = useOutletContext();
+  const user = useSelector(getUser);
 
   return (
     <>
@@ -12,7 +16,8 @@ const TermEditRoute = () => {
           {term?.names[0] || savedQuery || termId} - Słownik Biologiczny
         </title>
       </Helmet>
-      <TermForm term={term} />
+      { user ? <TermForm term={term} /> : <h2 className={styles.warning}> Musisz być zalogowany. </h2>}
+      
     </>
   );
 };
